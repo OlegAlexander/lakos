173c80ef93f796ae273099f7550c449abbd2dd10
OlegAlexander <42989765+OlegAlexander@users.noreply.github.com>
2020-04-27 23:00:42 -0700

    Refactored lakos tests.

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 A Dart tool for [modular programming](https://en.wikipedia.org/wiki/Modular_programming).
 
-* Visualize Dart library dependencies in Graphviz `dot`. 
-* Detect cyclic dependencies between libraries.
-* Compute the Normalized Cumulative Component Dependency (NCCD) metric.
+- Visualize Dart library dependencies in Graphviz `dot`.
+- Detect dependency cycles between libraries.
+- Identify orphan libraries.
+- Compute the Normalized Cumulative Component Dependency (NCCD) metric.
 
 The tool is named after John Lakos, author of Large-Scale C++ Software Design.
--- test/lakos_test.dart
+++ test/lakos_test.dart
@@ -1,6 +1,10 @@
 import 'dart:io' as io;
+import 'dart:convert' as convert;
 import 'package:test/test.dart';
 import 'package:pub_cache/pub_cache.dart' as pub_cache;
+import 'package:path/path.dart' as path;
+
+const outDir = 'dot_images';
 
 /// Return the location of the package on disk.
 /// Or return null if the package doesn't exist in the pub cache.
@@ -13,6 +17,26 @@ io.Directory getPackageLocation(String packageName, String packageVersion) {
     }
   }
   return null;
+}
+
+/// Run lakos dot and return the stdout.
+/// Also save the stdout to a dot file and generate a png.
+String runLakosDot(String rootDir, String outDir, String dotFilename) {
+  var result = io.Process.runSync('dart', ['bin/lakos.dart', 'dot', rootDir]);
+
+  io.File('$outDir/$dotFilename.dot').writeAsStringSync(result.stdout);
+  io.Process.runSync('dot', [
+    '-Tpng',
+    '$outDir/$dotFilename.dot',
+    '-Gdpi=300',
+    '-o',
+    '$outDir/$dotFilename.png'
+  ]);
+
+  // Remove carriage returns on Windows
+  var ls = convert.LineSplitter();
+  var lines = ls.convert(result.stdout);
+  return lines.join('\n');
 }
 
 void main() {
@@ -33,77 +57,64 @@ void main() {
   });
 
   test('getPackageLocation', () {
-    var gviz = getPackageLocation('gviz', '0.3.0');
-    expect(gviz, isNotNull);
-    gviz = getPackageLocation('gviz', '0.3.123');
-    expect(gviz, isNull);
-    gviz = getPackageLocation('gvizzz', '0.3.0');
-    expect(gviz, isNull);
+    var location = getPackageLocation('pub_cache', '0.2.3');
+    expect(location, isNotNull);
+    location = getPackageLocation('pub_cache', '0.2.12345');
+    expect(location, isNull);
+    location = getPackageLocation('pub_cacheeeeee', '0.2.3');
+    expect(location, isNull);
   });
 
   test('json_serializable', () {
     var packageLocation = getPackageLocation('json_serializable', '3.3.0');
-    expect(packageLocation, isNotNull);
-    var result = io.Process.runSync(
-        'dart', ['bin/lakos.dart', 'dot', packageLocation.path + '\\lib']);
-    print(result.stdout);
-    io.Process.runSync(
-        'dart',
-        [
-          'bin/lakos.dart',
-          'dot',
-          packageLocation.path + '\\lib',
-          '|',
-          'dot',
-          '-Tpng',
-          '-Gdpi=300',
-          '-o',
-          'dot_images/json_serializable.png'
-        ],
-        runInShell: true);
-  });
-
-  test('pub_cache', () {
-    var packageLocation = getPackageLocation('pub_cache', '0.2.3');
-    expect(packageLocation, isNotNull);
-    var result = io.Process.runSync(
-        'dart', ['bin/lakos.dart', 'dot', packageLocation.path]);
-    print(result.stdout);
-    io.Process.runSync(
-        'dart',
-        [
-          'bin/lakos.dart',
-          'dot',
-          packageLocation.path,
-          '|',
-          'dot',
-          '-Tpng',
-          '-Gdpi=300',
-          '-o',
-          'dot_images/pub_cache.png'
-        ],
-        runInShell: true);
+    var result = runLakosDot(
+        path.join(packageLocation.path, 'lib'), outDir, 'json_serializable');
+    print(result);
   });
 
   test('test', () {
     var packageLocation = getPackageLocation('test', '1.14.2');
-    expect(packageLocation, isNotNull);
-    var result = io.Process.runSync(
-        'dart', ['bin/lakos.dart', 'dot', packageLocation.path + '\\lib']);
-    print(result.stdout);
-    io.Process.runSync(
-        'dart',
-        [
-          'bin/lakos.dart',
-          'dot',
-          packageLocation.path + '\\lib',
-          '|',
-          'dot',
-          '-Tpng',
-          '-Gdpi=300',
-          '-o',
-          'dot_images/test.png'
-        ],
-        runInShell: true);
+    var result =
+        runLakosDot(path.join(packageLocation.path, 'lib'), outDir, 'test');
+    print(result);
+  });
+
+  test('pub_cache', () {
+    var packageLocation = getPackageLocation('pub_cache', '0.2.3');
+    var result = runLakosDot(packageLocation.path, outDir, 'pub_cache');
+    print(result);
+    expect(result, r'''
+digraph "G" {
+  label="";
+  labelloc=top;
+  style=rounded;
+  subgraph "cluster~\pub_cache-0.2.3" {
+    label="pub_cache-0.2.3";
+    subgraph "cluster~\pub_cache-0.2.3\example" {
+      label="example";
+      "\pub_cache-0.2.3\example\list.dart" [label="list"];
+    }
+    subgraph "cluster~\pub_cache-0.2.3\lib" {
+      label="lib";
+      "\pub_cache-0.2.3\lib\pub_cache.dart" [label="pub_cache"];
+      subgraph "cluster~\pub_cache-0.2.3\lib\src" {
+        label="src";
+        "\pub_cache-0.2.3\lib\src\impl.dart" [label="impl"];
+      }
+    }
+    subgraph "cluster~\pub_cache-0.2.3\test" {
+      label="test";
+      "\pub_cache-0.2.3\test\all.dart" [label="all"];
+      "\pub_cache-0.2.3\test\pub_cache_test.dart" [label="pub_cache_test"];
+    }
+    subgraph "cluster~\pub_cache-0.2.3\tool" {
+      label="tool";
+    }
+  }
+  "\pub_cache-0.2.3\lib\pub_cache.dart" -> "\pub_cache-0.2.3\lib\src\impl.dart";
+  "\pub_cache-0.2.3\lib\src\impl.dart" -> "\pub_cache-0.2.3\lib\pub_cache.dart";
+  "\pub_cache-0.2.3\test\all.dart" -> "\pub_cache-0.2.3\test\pub_cache_test.dart";
+}
+''');
   });
 }
