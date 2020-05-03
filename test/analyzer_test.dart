33f4b027948dacdabf55e4b02acc2845bb18a17b
OlegAlexander <42989765+OlegAlexander@users.noreply.github.com>
2020-05-03 15:56:59 -0700

    Started on resolve_imports.

--- test/analyzer_test.dart
+++ test/analyzer_test.dart
@@ -1,10 +1,20 @@
-// Conclusion: Use a combination of parseFile and package_config resolve.
+// Conclusion 5/2/20: Use a combination of parseFile and package_config resolve.
+// Conclusion 5/3/20: Don't use analyzer at all. It's too slow vs just parsing import statements.
+// Write your own resolver function using package_config.
 
+import 'dart:io';
+import 'dart:cli';
+import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/file_system/file_system.dart';
+import 'package:analyzer/source/package_map_resolver.dart';
 import 'package:test/test.dart';
 import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:analyzer/file_system/physical_file_system.dart';
+
 import 'package:analyzer/dart/analysis/utilities.dart';
 import 'package:analyzer/dart/analysis/features.dart';
 import 'package:package_config/package_config.dart';
+import 'package:path/path.dart';
 
 const filePath =
     r'C:\Users\olega\AppData\Roaming\Pub\Cache\hosted\pub.dartlang.org\analyzer-0.39.8\lib\dart\analysis\utilities.dart';
@@ -29,23 +39,39 @@ void main() {
     }
   });
 
+  test('AnalysisContextCollection getResolvedLibrary', () {
+    var includedPaths = [
+      r'C:\Users\olega\AppData\Roaming\Pub\Cache\hosted\pub.dartlang.org\test-1.14.2\lib\src\runner\browser\phantom_js.dart'
+    ];
+    var collection = AnalysisContextCollection(
+        includedPaths: includedPaths,
+        resourceProvider: PhysicalResourceProvider.INSTANCE);
+    for (var context in collection.contexts) {
+      print(['root', context.contextRoot.root]);
+      var currentSession = context.currentSession;
+      var resolvedLib = waitFor<ResolvedLibraryResult>(
+          currentSession.getResolvedLibrary(includedPaths[0]));
+      for (var lib in resolvedLib.element.importedLibraries) {
+        print(lib.identifier); // This doesn't resolve packages, only files
+      }
+      // var packageResolver = PackageMapUriResolver(context.contextRoot.resourceProvider, packageMap);
+    }
+  });
+
   test('parseFile', () {
     var parsedFile =
         parseFile(path: filePath, featureSet: FeatureSet.fromEnableFlags([]));
     for (var directive in parsedFile.unit.directives) {
-      print(directive.childEntities);
+      print([directive.runtimeType, directive.childEntities]);
     }
   });
 
-  test('PackageConfig', () async {
-    var packageConfig =
-        await findPackageConfigUri(Uri(scheme: 'file', path: filePath));
-    var packageUri =
-        Uri(scheme: 'package', path: 'analyzer/src/string_source.dart');
-    var fileUri = Uri(
-        scheme: 'file',
-        path:
-            'C:/Users/olega/AppData/Roaming/Pub/Cache/hosted/pub.dartlang.org/analyzer-0.39.8/lib/src/string_source.dart');
+  test('findPackageConfigUri package scheme', () async {
+    var uri = Uri(scheme: 'file', path: filePath);
+    var packageConfig = await findPackageConfigUri(uri);
+    var packageUri = Uri.parse('package:analyzer/src/string_source.dart');
+    var fileUri = Uri.parse(
+        'file:///C:/Users/olega/AppData/Roaming/Pub/Cache/hosted/pub.dartlang.org/analyzer-0.39.8/lib/src/string_source.dart');
     expect(packageConfig.resolve(packageUri), fileUri);
     expect(packageConfig.toPackageUri(fileUri), packageUri);
   });
